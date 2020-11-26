9e43c9ca80ff031a17eef306c620889a16990b89
ayin <317264314@qq.com>
2020-11-26 12:04:31 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,35 @@
+'''
+print("Hello World")
+print(2) #整数
+print(2.333) #小数
+print(False)  #布尔值
+print(()) #元组
+print([]) #数组
+print({}) #字典
+
+举头望明月
+低头思故乡
+
+print("哈哈"+"嘻嘻")
+print("女人"*50)
+'''
+
+"""
+a = input("请输入：")
+print("吃饭了吗:",a)
+
+print(type(123))
+print(type({}))
+
+print(type(int("233333")))
+print(type(str(333333)))
+
+
+a = input("请输入：")
+b = input("请再输入：")
+print(int(a)+int(b))
+"""
+
+a = '我是谁'
+b = '憨憨龟'
+print(len(a)+len(b))
