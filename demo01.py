cce14d63062ec8a7d66d1ec90dc5fb5ab51c5ca9
ayin <317264314@qq.com>
2020-11-26 20:38:28 +0800

    GoodNight

--- demo01.py
+++ demo01.py
@@ -28,9 +28,49 @@ print(type(str(333333)))
 a = input("请输入：")
 b = input("请再输入：")
 print(int(a)+int(b))
-"""
+
 
 a = '我是谁'
 b = '憨憨龟'
 print(len(a)+len(b))
 print(123)
+
+
+#元组
+a = (1,2,3,4,5,5,5)
+#print(a[4])
+#print(a.index(3)) #位置
+#print(a.count(5)) #计数
+
+#二维元组
+#b = (a,'哈哈','嘻嘻') 
+#print(b[0][1]) 
+
+#切片
+#print(a[0:4])
+print(a[0:])
+"""
+
+#数组
+#a = [1,2,3,4,5,5,5]
+#a[0] = 0 修改
+#a.append('append') 添加
+#a.insert(0,'insert') 往固定位置插入
+#b = a.pop(6) 剪切数据
+#a.clear() 清空
+#xx = ['蒜头王八','憨憨龟']
+#a.extend(xx) 合并其他数组
+#a.remove(5) 删除某个值
+#print(a[0:])
+
+#字典
+a = {
+    "name":"蒜头王八",
+    'sex':'未知',
+    'age':'10'}
+
+a["hight"] = '183cm' #新增
+a["sex"] = 'man' #修改
+#a.update(sex='woman') 修改
+print(a["name"],a['hight'],a['sex']) #取值
+
